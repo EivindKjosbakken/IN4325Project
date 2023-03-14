067577d87a4c4c924c0ba1c70e0b8454e66ea1ff
JuNicky <yaohua@live.nl>
2023-03-14 14:49:53 +0100

    get request

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div>
     <input v-model="query" placeholder="enter query" />
     <p>Query is: {{ query }}</p>
+    <button @click="recieve">beautiful button</button>
   </div>
 </template>
 
@@ -10,6 +11,20 @@
     data() {
       return {
         query: ''
+      }
+    },
+    methods: {
+      async recieve() {
+        const response = await fetch("http://localhost:5000/testGet");
+        const data = await response.json();
+        console.log(data)
+        // try {
+        //   fetch("http://localhost:5000/testGet")
+        //   .then(response => response.json())
+        //   .then(data => (console.log(data)));
+        // } catch (e) {
+        //   console.log(e)
+        // }
       }
     }
   }
