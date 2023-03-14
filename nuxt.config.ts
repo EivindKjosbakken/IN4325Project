d8e02735a944365c9956054513670323d828008f
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2023-03-14 16:41:38 +0000

    added proxy to avoid CORS error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	devServer: {
+		proxy: {
+		  '^/api': {
+			target: 'http://localhost:3000',
+			changeOrigin: true
+		  },
+		}
+	  }
 })
