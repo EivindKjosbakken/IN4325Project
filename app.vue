baebf0f0724dd9a3612e58b1c54fe83e06fd384d
JuNicky <yaohua@live.nl>
2023-03-14 14:07:21 +0100

    init project

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input v-model="query" placeholder="enter query" />
+    <p>Query is: {{ query }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        query: ''
+      }
+    }
+  }
+</script>
