055765671a272893141f3f9df368bd58cab6c2e4
JuNicky <yaohua@live.nl>
2023-03-15 14:58:13 +0100

    seperate backend and script to run both:

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Run npm and Python in the background and capture their PIDs
-npm run dev &
-npm_pid=$!
-python run.py &
+cd backend && python run.py &
 python_pid=$!
+cd frontend && npm run dev &
+npm_pid=$!
+
 
 # Wait for both processes to finish
 wait $npm_pid
