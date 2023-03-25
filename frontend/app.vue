619e356c8eaa6dd57ebead701d66a7b4d4dbc7e2
JuNicky <yaohua@live.nl>
2023-03-25 20:59:10 +0100

    filter

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <div class="search-container">
       <div class="search-input">
         <img src="./assets/GoogIR.png" width="150"/>
@@ -9,9 +9,18 @@
         </div>
       </div>
     </div>
-    <div class="search-query" v-if="timeTaken !== -1">
-      <p>Time elapsed: {{ timeTaken }} seconds</p>
+    <div class="filters" >
+      <div class="dropdown">
+        <button class="dropbtn">Filter by:</button>
+        <div class="dropdown-content">
+          <label><input type="checkbox" v-model="filters" value="Faculty">Faculty</label>
+          <label><input type="checkbox" v-model="filters" value="More options">More options</label>
+          <label><input type="checkbox" v-model="filters" value="Idk">Idk</label>
+        </div>
+      </div>
+      <p v-if="timeTaken !== -1">Time elapsed: {{ timeTaken }} seconds</p>
     </div>
+    
     <LoadingScreen v-if="isLoading" />
     <div class="search-result" v-else v-for="item in data">
       <a :href="item[1]" target="_blank">
@@ -32,6 +41,7 @@ export default {
       maxAbstractWords: 50,
       timeTaken: -1,
       isLoading: false,
+      filters: [],
       // Data index meanings
       // 0: uuid, 1: repository link, 2: title, 3: author, 4: contributor, 5: publication year, 6: abstract, 7: subject topic,
       // 8: language, 9: publication type, 10: publisher, 11: isbn, 12: issn, 13: patent, 14: patent status, 15: bibliographic note,
@@ -107,7 +117,6 @@ export default {
 .search-bar {
   width: 80%;
   margin-left: 25px;
-  /* margin-top: -100px; */
   border: none;
   font-size: 16px;
   padding: 10px;
@@ -135,8 +144,8 @@ export default {
   background-color: #2563eb;
 }
 
-.search-query {
-  color: #808080;
+.filters {
+  display: flex;
   font-size: 12px;
   max-width: 1000px;
   margin: 0 auto;
@@ -163,5 +172,47 @@ export default {
   font-size: 1.2rem;
   color: #808080;
   margin-top: 0.5rem;
+}
+
+.dropdown {
+  margin-left: 10px;
+  margin-right: 20px;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  z-index: 1;
+  background-color: #fff;
+  min-width: 160px;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.dropdown-content label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+button {
+  font-size: 16px;
+  color: #fff;
+  background-color: #3b82f6;
+  border-radius: 5px;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-family: unset
+}
+
+button:hover {
+  background-color: #2563eb;
 }
 </style>
