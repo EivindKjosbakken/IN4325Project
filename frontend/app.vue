8a60ab2a3c9a1d14a71104a12a8fbc37e8d2ef50
JuNicky <yaohua@live.nl>
2023-03-25 19:59:58 +0100

    add loading screen

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,76 +1,88 @@
 <template>
-  <div class="search-container">
-    <div class="search-input">
-      <input class="search-bar" v-model="query" v-on:keyup.enter="fetch(query)" placeholder="Search for something" />
+  <div>
+    <div class="search-container">
+      <div class="search-input">
+        <img src="./assets/GoogIR.png" width="150"/>
+        <input class="search-bar" v-model="query" v-on:keyup.enter="fetch(query)" placeholder="Search for something" />
+        <div class="search-button">
+          <button class="button-primary" @click="fetch(query)">Search</button>
+        </div>
+      </div>
     </div>
-    <div class="search-button">
-      <button class="button-primary" @click="fetch(query)">Search</button>
+    <div class="search-query" v-if="timeTaken !== -1">
+      <p>Time elapsed: {{ timeTaken }} seconds</p>
     </div>
-  </div>
-  
-  <div class="search-query" v-if="timeTaken !== -1">
-    <p>Time elapsed: {{ timeTaken }} seconds</p>
-  </div>
-  <div class="search-result" v-for="item in data">
-    <a :href="item[1]" target="_blank">
-      <h3 class="search-result-title" @click='window.location.href = item[1]'>{{ item[2] }}</h3>
-    </a>
-    <p class="search-result-abstract">{{ getLimitedAbstract(item[6]) }}</p>
+    <LoadingScreen v-if="isLoading" />
+    <div class="search-result" v-else v-for="item in data">
+      <a :href="item[1]" target="_blank">
+        <h3 class="search-result-title" @click='window.location.href = item[1]'>{{ item[2] }}</h3>
+      </a>
+      <p class="search-result-abstract">{{ getLimitedAbstract(item[6]) }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        query: '',
-        maxAbstractWords: 50,
-        timeTaken: -1,
-        // Data index meanings
-        // 0: uuid, 1: repository link, 2: title, 3: author, 4: contributor, 5: publication year, 6: abstract, 7: subject topic,
-        // 8: language, 9: publication type, 10: publisher, 11: isbn, 12: issn, 13: patent, 14: patent status, 15: bibliographic note,
-        // 16: access restriction, 17: embargo date, 18: faculty, 19: department, 20: research group, 21: programme, 22: project, 23: coordinates
-        data: '',
-        
-      }
-    },
-    methods: {
-      async fetch(query) {
-        const options = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ 'query': query })
-        };
+import LoadingScreen from "./LoadingScreen";
 
-        fetch('http://localhost:5000/retrieve', options)
-          .then(async response => {
-            const data = await response.json();
-            if (!response.ok) {
-              const error = (data && data.message) || response.status;
-              return Promise.reject(error);
-            }
-            this.timeTaken = data.time;
-            this.data = data.results;
-          })
-          .catch(error => {
-            this.errorMessage = error;
-            console.error('There was an error!', error);
-          });
-      }
-    },
-    computed: {
-      getLimitedAbstract() {
-        return function(abstract) {
-          const words = abstract.split(" ");
-          if (words.length > this.maxAbstractWords) {
-            return words.slice(0, this.maxAbstractWords).join(" ") + " ...";
-          } else {
-            return abstract;
-          }
-        };
-      }
+export default {
+  data() {
+    return {
+      query: '',
+      maxAbstractWords: 50,
+      timeTaken: -1,
+      isLoading: false,
+      // Data index meanings
+      // 0: uuid, 1: repository link, 2: title, 3: author, 4: contributor, 5: publication year, 6: abstract, 7: subject topic,
+      // 8: language, 9: publication type, 10: publisher, 11: isbn, 12: issn, 13: patent, 14: patent status, 15: bibliographic note,
+      // 16: access restriction, 17: embargo date, 18: faculty, 19: department, 20: research group, 21: programme, 22: project, 23: coordinates
+      data: '',
+      
     }
+  },
+  methods: {
+    async fetch(query) {
+      this.isLoading = true;
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'query': query })
+      };
+      
+      fetch('http://localhost:5000/retrieve', options)
+        .then(async response => {
+          const data = await response.json();
+          if (!response.ok) {
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+          }
+          this.timeTaken = data.time;
+          this.data = data.results;
+          this.isLoading = false;
+        })
+        .catch(error => {
+          this.errorMessage = error;
+          this.isLoading = false;
+          console.error('There was an error!', error);
+        });
+    }
+  },
+  computed: {
+    getLimitedAbstract() {
+      return function(abstract) {
+        const words = abstract.split(" ");
+        if (words.length > this.maxAbstractWords) {
+          return words.slice(0, this.maxAbstractWords).join(" ") + " ...";
+        } else {
+          return abstract;
+        }
+      };
+    }
+  },
+  components: {
+    LoadingScreen
   }
+}
 </script>
 
 <style>
@@ -88,11 +100,14 @@
 
 .search-input {
   flex: 1;
-  margin-right: 10px;
+  display: flex;
+  align-items: center;
 }
 
 .search-bar {
-  width: 100%;
+  width: 80%;
+  margin-left: 25px;
+  /* margin-top: -100px; */
   border: none;
   font-size: 16px;
   padding: 10px;
